02924f0716cf1e09ff23a814536001c616f89186
Egor <bioshoker88@gmail.com>
2023-08-17 08:25:22 +0200

    update

--- respond/admin.py
+++ respond/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Respondent, Email, Phone
+from .models import Respondent
 from import_export.admin import ImportExportModelAdmin
 
 
@@ -11,18 +11,11 @@ from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Respondent)
 class RespondentAdmin(ImportExportModelAdmin):
-    list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
+    list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'phone_double', 'email', 'email_double')
     #list_filter = ('phone','full_name')
-    search_fields = ('okpo', 'inn', 'full_name', 'okved')
+    search_fields = ('okpo', 'inn', 'full_name', 'okved', 'email', 'phone', 'email_double')
 
 
 
 
-@admin.register(Email)
-class EmailAdmin(ImportExportModelAdmin):
-  pass
 
-
-@admin.register(Phone)
-class PhoneAdmin(ImportExportModelAdmin):
-  pass
--- respond/models.py
+++ respond/models.py
@@ -4,24 +4,28 @@ from django.db import models
 
 
 
-class Email(models.Model):
-    address = models.EmailField(max_length=100, verbose_name='Адрес почты ', blank=True, null=True)
-
-    def __str__(self):
-        return self.address
-    class Meta:
-        verbose_name_plural = 'Электронная почта'
 
 
-class Phone(models.Model):
-    address = models.TextField(max_length=100, verbose_name='Номер телефона ', blank=True, null=True)
 
-    def __str__(self):
-        return self.address
 
-    class Meta:
-        verbose_name_plural = 'Телефон'
-
+# class Email(models.Model):
+#     address = models.EmailField(max_length=200, verbose_name='Адрес почты ', blank=True, null=True)
+#
+#     def __str__(self):
+#         return self.address
+#     class Meta:
+#         verbose_name_plural = 'Электронная почта'
+#
+#
+# class Phone(models.Model):
+#     address = models.TextField(max_length=100, verbose_name='Номер телефона ', blank=True, null=True)
+#
+#     def __str__(self):
+#         return self.address
+#
+#
+#     class Meta:
+#         verbose_name_plural = 'Телефон'
 
 
 class Respondent(models.Model):
@@ -30,9 +34,14 @@ class Respondent(models.Model):
     full_name = models.TextField(max_length=250, verbose_name='Полное наименование')
     okved = models.CharField(max_length=20, verbose_name='ОКВЭД')
     inn = models.CharField(max_length=20, verbose_name='ИНН', null=True)
-    phone = models.ForeignKey(Phone, on_delete=models.CASCADE, verbose_name='Телефон', related_name='respondents', null=True)
-    email = models.ForeignKey(Email,  on_delete=models.CASCADE,  verbose_name='Email',  related_name='respondents', null=True)
+    phone = models.CharField(max_length=150, verbose_name='Телефон', null=True)
+    phone_double = models.CharField(max_length=150, verbose_name='Дополнительный телефон', blank=True, null=True)
+    email = models.EmailField(max_length=200,  verbose_name='Email',blank=True,   null=True)
+    email_double = models.EmailField(max_length=200, verbose_name="Дополнительный email", blank=True, null=True)
+
 
     class Meta:
         verbose_name_plural = 'Респонденты'
+
+
 
