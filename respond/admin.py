55895ca46ea06014c101169296d451b706b30be6
Egor <bioshoker88@gmail.com>
2023-08-18 11:00:36 +0200

    update

--- respond/admin.py
+++ respond/admin.py
@@ -1,19 +1,46 @@
 from django.contrib import admin
-from .models import Respondent
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from .models import Respondent
 
+class RespondentResource(resources.ModelResource):
+    class Meta:
+        model = Respondent
 
-
-
-
-
-
-
-@admin.register(Respondent)
 class RespondentAdmin(ImportExportModelAdmin):
+    resource_class = RespondentResource
     list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'phone_double', 'email', 'email_double')
-    #list_filter = ('phone','full_name')
-    search_fields = ('okpo', 'inn', 'full_name', 'okved', 'email', 'phone', 'email_double')
+    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'phone_double', 'email', 'email_double')
+
+    def import_data(self, dataset, dry_run=False, **kwargs):
+        # Импорт данных из файла
+        result = super().import_data(dataset, dry_run, **kwargs)
+
+        if not dry_run:
+            for row in dataset.dict:
+                okpo = row['okpo']
+                email = row['email']
+                phone = row['phone']
+
+                try:
+                    # Попытка получить существующего респондента с указанным okpo
+                    respondent = Respondent.objects.get(okpo=okpo)
+
+                    # Обновление полей email и phone у найденного респондента
+                    respondent.email = email
+                    respondent.phone = phone
+                    respondent.save()
+                except Respondent.DoesNotExist:
+                    # Если респондент с указанным okpo не найден, продолжить обработку следующей записи
+                    pass
+
+            self.message_user(request, "Successfully updated {result.totals['updated']} respondents.")
+
+        return result
+
+admin.site.register(Respondent, RespondentAdmin)
+
+
 
 
 
--- respond/models.py
+++ respond/models.py
@@ -8,29 +8,11 @@ from django.db import models
 
 
 
-# class Email(models.Model):
-#     address = models.EmailField(max_length=200, verbose_name='Адрес почты ', blank=True, null=True)
-#
-#     def __str__(self):
-#         return self.address
-#     class Meta:
-#         verbose_name_plural = 'Электронная почта'
-#
-#
-# class Phone(models.Model):
-#     address = models.TextField(max_length=100, verbose_name='Номер телефона ', blank=True, null=True)
-#
-#     def __str__(self):
-#         return self.address
-#
-#
-#     class Meta:
-#         verbose_name_plural = 'Телефон'
 
 
 class Respondent(models.Model):
     objects = models.Manager()
-    okpo = models.CharField(max_length=20, verbose_name='ОКПО', unique=True)
+    okpo = models.CharField(max_length=20, verbose_name='ОКПО',unique=True)
     full_name = models.TextField(max_length=250, verbose_name='Полное наименование')
     okved = models.CharField(max_length=20, verbose_name='ОКВЭД')
     inn = models.CharField(max_length=20, verbose_name='ИНН', null=True)
@@ -43,5 +25,9 @@ class Respondent(models.Model):
     class Meta:
         verbose_name_plural = 'Респонденты'
 
+    @classmethod
+    def update_or_create_with_same_okpo(cls, okpo, **kwargs):
+        respondent, created = cls.objects.update_or_create(okpo=okpo, defaults=kwargs)
+        return respondent, created
 
 
