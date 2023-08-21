d435e4fc7cf19c905cb39e81fa7b5042406735b3
Egor <bioshoker88@gmail.com>
2023-08-21 12:28:02 +0200

    update

--- respond/admin.py
+++ respond/admin.py
@@ -1,48 +1,19 @@
-from django.contrib import admin
-from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import Respondent
+from django.contrib import admin
 
-class RespondentResource(resources.ModelResource):
-    class Meta:
-        model = Respondent
+from .models import Respondent,Phone, Email
+
 
 class RespondentAdmin(ImportExportModelAdmin):
-    resource_class = RespondentResource
-    list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'phone_double', 'email', 'email_double')
-    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'phone_double', 'email', 'email_double')
+    list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
+    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
 
-    def import_data(self, dataset, dry_run=False, **kwargs):
-        # Импорт данных из файла
-        result = super().import_data(dataset, dry_run, **kwargs)
+class PhoneAdmin(ImportExportModelAdmin):
+    list_display = ('phone_number',)
 
-        if not dry_run:
-            for row in dataset.dict:
-                okpo = row['okpo']
-                email = row['email']
-                phone = row['phone']
-
-                try:
-                    # Попытка получить существующего респондента с указанным okpo
-                    respondent = Respondent.objects.get(okpo=okpo)
-
-                    # Обновление полей email и phone у найденного респондента
-                    respondent.email = email
-                    respondent.phone = phone
-                    respondent.save()
-                except Respondent.DoesNotExist:
-                    # Если респондент с указанным okpo не найден, продолжить обработку следующей записи
-                    pass
-
-            self.message_user(request, "Successfully updated {result.totals['updated']} respondents.")
-
-        return result
+class EmailAdmin(ImportExportModelAdmin):
+    list_display = ('email_address',)
 
 admin.site.register(Respondent, RespondentAdmin)
-
-
-
-
-
-
-
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Email, EmailAdmin)
--- respond/models.py
+++ respond/models.py
@@ -1,33 +1,38 @@
 from django.db import models
 
-
-
-
-
-
-
-
-
-
-
 class Respondent(models.Model):
     objects = models.Manager()
-    okpo = models.CharField(max_length=20, verbose_name='ОКПО',unique=True)
+    okpo = models.CharField(max_length=20, verbose_name='ОКПО', unique=True)
     full_name = models.TextField(max_length=250, verbose_name='Полное наименование')
     okved = models.CharField(max_length=20, verbose_name='ОКВЭД')
     inn = models.CharField(max_length=20, verbose_name='ИНН', null=True)
-    phone = models.CharField(max_length=150, verbose_name='Телефон', null=True)
-    phone_double = models.CharField(max_length=150, verbose_name='Дополнительный телефон', blank=True, null=True)
-    email = models.EmailField(max_length=200,  verbose_name='Email',blank=True,   null=True)
-    email_double = models.EmailField(max_length=200, verbose_name="Дополнительный email", blank=True, null=True)
-
+    phone = models.ForeignKey('Phone', on_delete=models.CASCADE, verbose_name='Телефон', related_name='respondent', null=False, blank=True)
+    email = models.ForeignKey('Email', on_delete=models.CASCADE, verbose_name='Email', related_name='respondent', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Респонденты'
 
-    @classmethod
-    def update_or_create_with_same_okpo(cls, okpo, **kwargs):
-        respondent, created = cls.objects.update_or_create(okpo=okpo, defaults=kwargs)
-        return respondent, created
+class Phone(models.Model):
+    phone_number = models.CharField(max_length=150, verbose_name='Телефон', null=False, blank=True)
 
+
+
+    class Meta:
+        verbose_name_plural = 'Телефоны'
+
+class Email(models.Model):
+    email_address = models.EmailField(max_length=200, verbose_name='Email')
+
+
+
+    class Meta:
+        verbose_name_plural = 'Электронный адрес'
+
+
+
+    #
+    # @classmethod
+    # def update_or_create_with_same_okpo(cls, okpo, **kwargs):
+    #     respondent, created = cls.objects.update_or_create(okpo=okpo, defaults=kwargs)
+    #     return respondent, created
 
