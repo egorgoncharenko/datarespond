443c874a6172305baa3c8ff646331d15d7ab57a8
Egor <bioshoker88@gmail.com>
2023-08-30 11:13:21 +0200

    update

--- respond/models.py
+++ respond/models.py
@@ -6,8 +6,8 @@ class Respondent(models.Model):
     full_name = models.TextField(max_length=250, verbose_name='Полное наименование')
     okved = models.CharField(max_length=20, verbose_name='ОКВЭД')
     inn = models.CharField(max_length=20, verbose_name='ИНН', null=True)
-    phone = models.ForeignKey('Phone', on_delete=models.CASCADE, verbose_name='Телефон', related_name='respondent', null=False, blank=True)
-    email = models.ForeignKey('Email', on_delete=models.CASCADE, verbose_name='Email', related_name='respondent', null=True, blank=True)
+    phone = models.ManyToManyField('Phone', on_delete=models.CASCADE, verbose_name='Телефон', related_name='respondent', null=False, blank=True)
+    email = models.For('Email', on_delete=models.CASCADE, verbose_name='Email', related_name='respondent', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Респонденты'
