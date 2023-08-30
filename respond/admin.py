fb29ecf48602a22975edcf9c7b86ecaf14c48cca
Egor <bioshoker88@gmail.com>
2023-08-30 11:01:18 +0200

    update

--- respond/admin.py
+++ respond/admin.py
@@ -1,21 +1,38 @@
-from django.contrib import admin
-from .models import Respondent
 from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
+
+from .models import Respondent, Phone, Email
 
 
-
-
-
-
-
-@admin.register(Respondent)
 class RespondentAdmin(ImportExportModelAdmin):
-    list_display = ('okpo', 'full_name', 'okved', 'inn',   'email', 'phone')
-    search_fields = ('okpo', 'inn', 'full_name', 'okved', 'phone')
+    list_display = ('okpo', 'full_name', 'okved', 'inn', 'display_phones', 'display_emails')
+    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
+
+    def display_phones(self, obj):
+        phones = obj.phone.all()
+        return ", ".join([phone.phone_number for phone in phones])
+
+    display_phones.short_description = 'Телефоны'
+
+    def display_emails(self, obj):
+        emails = obj.email.all()
+        return ", ".join([email.email_address for email in emails])
+
+    display_emails.short_description = 'Emails'
 
 
 
 
-#@admin.register(Phone)
-#class RespondentAdmin(ImportExportModelAdmin):
-    #pass
+
+
+
+class PhoneAdmin(ImportExportModelAdmin):
+    list_display = ('phone_number', 'okpo')
+
+
+class EmailAdmin(ImportExportModelAdmin):
+    list_display = ('email_address',)
+
+admin.site.register(Respondent, RespondentAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Email, EmailAdmin)
--- respond/models.py
+++ respond/models.py
@@ -1,27 +1,13 @@
 from django.db import models
 
-
-
-# class Phone(models.Model):
-#
-#     phone = models.IntegerField(  verbose_name='Телефон', blank=True)
-#
-#     def __str__(self):
-#         return self.phone
-#
-#     class Meta:
-#         verbose_name_plural = 'Телефоны'
-
 class Respondent(models.Model):
     objects = models.Manager()
-    okpo = models.CharField(max_length=20, verbose_name='ОКПО', unique=True)
-    full_name = models.CharField(max_length=200, verbose_name='Полное наименование')
-    okved = models.CharField(max_length=20, verbose_name='ОКВЭД')
-    inn = models.CharField(max_length=20, verbose_name='ИНН', null=True)
-    phone = models.CharField(max_length=50, verbose_name='Телефон', blank=True)
-    email = models.EmailField(max_length=100, verbose_name='Email', blank=True, null=False)
-
-
+    okpo = models.CharField(max_length=200, verbose_name='ОКПО')
+    full_name = models.TextField(max_length=200, verbose_name='Полное наименование')
+    okved = models.CharField(max_length=50, verbose_name='ОКВЭД')
+    inn = models.CharField(max_length=50, verbose_name='ИНН', null=True)
+    phone = models.ManyToManyField('Phone',   verbose_name='Телефон', related_name='respondent')
+    email = models.ManyToManyField('Email',  verbose_name='Email', related_name='respondent')
 
 
 
@@ -29,3 +15,28 @@ class Respondent(models.Model):
         verbose_name_plural = 'Респонденты'
 
 
+
+class Phone(models.Model):
+    phone_number = models.TextField(max_length=150, verbose_name='Телефон')
+    okpo = models.CharField(max_length=200, verbose_name='ОКПО')
+
+    def __str__(self):
+          return  self.phone_number
+
+
+    class Meta:
+        verbose_name_plural = 'Телефоны'
+
+
+
+
+
+class Email(models.Model):
+    email_address = models.EmailField(max_length=200, verbose_name='Email')
+
+    def __str__(self):
+          return  self.email_address
+
+
+    class Meta:
+        verbose_name_plural = 'Электронный адрес'
