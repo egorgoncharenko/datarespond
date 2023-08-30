a8c409900f1b3f5d416d90371cd90adb1a273709
Egor <bioshoker88@gmail.com>
2023-08-30 11:26:12 +0200

    update

--- respond/admin.py
+++ respond/admin.py
@@ -5,8 +5,20 @@ from .models import Respondent,Phone, Email
 
 
 class RespondentAdmin(ImportExportModelAdmin):
-    list_display = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
-    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'phone', 'email')
+    list_display = ('okpo', 'full_name', 'okved', 'inn', 'display_phones', 'display_emails')
+    search_fields = ('okpo', 'full_name', 'okved', 'inn', 'display_phones', 'display_emails')
+
+    def display_phones(self, obj):
+        phones = obj.phone.all()
+        return ", ".join([phone.phone_number for phone in phones])
+
+    display_phones.short_description = 'Телефоны'
+
+    def display_emails(self, obj):
+        emails = obj.email.all()
+        return ", ".join([email.email_address for email in emails])
+
+    display_emails.short_description = 'Emails'
 
 class PhoneAdmin(ImportExportModelAdmin):
     list_display = ('phone_number',)
